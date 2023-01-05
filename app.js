89ccc8825ece2184524816a6533a8234ad4378eb
farmerick <78766749+farmerick@users.noreply.github.com>
2023-01-05 15:11:58 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -134,7 +134,8 @@ app.post("/delete", function(req, res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+const port = process.env.PORT || 3000;
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
